21059e16d1f8533e852c632d44b3cf0a6c499ee2
stephenkinyua <kinyuamunene.km@gmail.com>
2022-08-31 15:48:48 +0300

    refactor: consolidate all listing blade templates

---
+++ resources/views/listings/index.blade.php
@@ -0,0 +1,33 @@
+<x-layout>
+    @include('partials._hero')
+    @include('partials._search')
+
+    <div class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4">
+
+
+        <!-- Unless Directive -->
+        @unless (count($listings) === 0)
+
+        <!-- For each Directive -->
+        @foreach ($listings as $listing)
+
+        <!-- Use the component -->
+        <x-listing-card :listing="$listing" />
+
+        <p>{{ $listing['description'] }}</p>
+        @endforeach
+
+        @else
+        <p>No listings found!</p>
+        @endunless
+
+
+    </div>
+
+    <!-- If Directive -->
+    <!-- @if (count($listings) == 0)
+<p>No listings found!</p>
+@endif -->
+
+
+</x-layout>
