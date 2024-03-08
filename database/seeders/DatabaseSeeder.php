51c85c9841a882c148de6ffe2547404cbf2ff030
Stephen Kinyua Munene <kinyuamunene.km@gmail.com>
2024-03-08 16:25:27 +0300

    Complete project Skeleton (#1)
    * add example routes
    
    * add a view and pass data down to the view
    
    * substitute regular php for blade template
    
    use laravel blade directives
    
    * setup postgres database
    
    * switch to sqlite
    
    * seed data and use eloquent to create db models
    
    * clean and add notes to readme file
    
    * create model factory
    
    * create listing model factory
    
    * setup base layout template
    
    * upddate: add tailwind css
    
    * create tags and listing components
    
    * setup listing controller
    
    * refactor: consolidate all listing blade templates
    
    * add filtering and search functionality
    
    * add listing create functionality
    
    Subfeatures
    1. Disappearing flash messages
    2. New routes to handle form display and listing creation including validation
    3. Add clock work library to help with debugging and visualizing queries
    
    * add pagination
    
    * add form file upload
    
    * add edit and delete functionality
    
    * add user authentication
    
    * add route guarding
    
    * chore: update packages & remove broken link

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Listing;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,7 +16,21 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(10)->create();
+
+
+        // Listing::create([
+        //     'title' => 'Laravel Senior Developer',
+        //     'tags' => 'laravel, javascript',
+        //     'company' => 'Acme Corp',
+        //     'location' => 'Boston, MA',
+        //     'email' => 'email1@email.com',
+        //     'website' => 'https://www.acme.com',
+        //     'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam minima et illo reprehenderit quas possimus voluptas repudiandae cum expedita, eveniet aliquid, quam illum quaerat consequatur! Expedita ab consectetur tenetur delensiti?'
+        // ],);
+
+        Listing::factory(6)->create();
+
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
